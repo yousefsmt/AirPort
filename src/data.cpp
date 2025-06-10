d88bb43a1fd6460b8f613a2c575434d9f78cb8b3
Yousef Samet <yoosefsamet@gmail.com>
2025-06-10 21:06:21 +0330

    Initial commit

---
+++ src/data.cpp
@@ -0,0 +1,24 @@
+#include "data.h"
+
+void Date::setDay(int& d){}
+int Date::getDay(){}
+
+void Date::setMonth(int& d){}
+int Date::getMonth(){}
+
+void Date::setYear(int& d){}
+int Date::getYear(){}
+
+void Date::setHour(int& d){}
+int Date::getHour(){}
+
+void Date::setMinute(int& d){}
+int Date::getMinute(){}
+
+void Food::setType(const std::string& t){}
+std::string Food::getType(){}
+
+void Food::setMeal(const std::string& g){}
+std::string Food::getMeal(){}
+
+
